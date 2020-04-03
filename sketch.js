473704bf3eca6d7b925e1ed39c6798b7acaa01f8
Muralidharan69 <62016575+Muralidharan69@users.noreply.github.com>
2020-04-03 14:28:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(800,400);
+}
+
+function draw() {
+  background("black");
+  rect(380, 200, 100, 280);
+ 
+  circle(430,165,70);
+  fill("white")
+  rect(490,150,70,370);
+  fill("white")
+  rect(300,150,70,370);
+  fill("white")
+  rect(100,150,70,450);
+  fill("white")
+  rect(670,150,70,450);
+  fill("white")
+  circle(530,115,70);
+  fill("white")
+  circle(340,115,70);
+  fill("white")
+  circle(130,115,70);
+  fill("white")
+  circle(700,115,70);
+  fill("white")   
+  drawSprites();
+}
